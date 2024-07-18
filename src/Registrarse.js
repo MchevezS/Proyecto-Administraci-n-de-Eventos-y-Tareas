ee99557090a16f1b531200b9346606861433c195
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-18 14:24:03 -0600

    variables de nombre, email, password, btnRegistrarse

--- src/Registrarse.js
+++ src/Registrarse.js
@@ -1,4 +1,7 @@
-
+let nombre = document.getElementById("nombre")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let btnRegistrarse = document.getElementById("btnRegistrarse")
 
 
 
