e48e0e2f16a9c2d8306e0b5d8ec724d4cb15bae3
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-18 15:34:08 -0600

    valide los espacios vacios, guarde los datos en el localStorage, le di funcion al boton registrarse

--- src/Registrarse.js
+++ src/Registrarse.js
@@ -1,14 +1,29 @@
-let nombre = document.getElementById("nombre")
-let email = document.getElementById("email")
-let password = document.getElementById("password")
 let btnRegistrarse = document.getElementById("btnRegistrarse")
-function guardarDatos(params) {
-    localStorage.setItem("nombre","nombre")
-    localStorage.setItem("email", "email")
-    localStorage.setItem("password","password")   
-}
-addEventListener
+let nombre = document.getElementById("nombre").value
+let email = document.getElementById("email").value
+let password = document.getElementById("password").value
 
+function validarVacios() {         // Estoy validando espacios vacios
+    if (nombre.trim()===""|| email.trim()==="" || password.trim()==="") {
+        alert('llenar espacios vacios') // cambiar alerta
+    }else{
+        guardarDatos()
+    }
+}
+function guardarDatos() {   
+    let nombre = document.getElementById("nombre").value // con el .value les estoy dando un valor
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+     // Aca estoy guardando los datos en el localStorage
+    localStorage.setItem("nombre",nombre),
+    localStorage.setItem("email", email),
+    localStorage.setItem("password",password)   
+    // para que los datos no se elimine del localStorage lo tengo que hacer con un arreglo
+}
+  // Aca estoy dandole un evento al boton
+btnRegistrarse.addEventListener("click",
+    validarVacios
+)
 
 
 
