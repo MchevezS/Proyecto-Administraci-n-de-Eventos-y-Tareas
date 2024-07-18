afede0a42248ab3f12df35ebef5089534786ec83
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-18 14:39:00 -0600

    estoy haciendo una funcion para guardar los datos de registrarse

--- src/Registrarse.js
+++ src/Registrarse.js
@@ -2,9 +2,12 @@ let nombre = document.getElementById("nombre")
 let email = document.getElementById("email")
 let password = document.getElementById("password")
 let btnRegistrarse = document.getElementById("btnRegistrarse")
-
-
-
+function guardarDatos(params) {
+    localStorage.setItem("nombre","nombre")
+    localStorage.setItem("email", "email")
+    localStorage.setItem("password","password")   
+}
+addEventListener
 
 
 
