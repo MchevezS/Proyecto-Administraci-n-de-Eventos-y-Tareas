2b021ad0e8b8695cb318bceb4397ac256d0d1d61
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-19 16:29:14 -0600

    En el login de html solo hice cambios. En el login de js: hice que me apareciera los daatos de mi registro, le agregue validades cuando haya espacios vacios. En mi registrarse de html solo le hice cambios. En mi registrarse de js, hice que se me guardaran los datos que yo ingreso en caada input, luego le agregue las validaciones de los espacios vacios

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,43 @@
-let Email = document.getElementById("email")
+let emailL = document.getElementById("emailL")
+let password = document.getElementById("password")
+let btnInicio = document.getElementById("btnInicio")
 
 
+// voy a comenzar a traer los datos del LocalStorage
+function traerDatos() {
+    let contrasena=password.value
+    let lemail=emailL.value
+    let emailLocal=localStorage.getItem("email",emailL)
+    let passwordLocal=localStorage.getItem("password",password)
+    console.log(emailLocal+" local storage email");
+    console.log(passwordLocal+" local storage email");
 
+    if (lemail===emailLocal&&contrasena===passwordLocal) {
+        alert('Entramos exitosamente')
+    }else{
+        alert('incorrecto')
+    }
+}
 
+// Valide espacios vacios
+function validarVacios() {    
+    let emailL = document.getElementById("emailL").value 
+    let password = document.getElementById("password").value
+    // estas dos vaariables las agregue porque no me dejaba validar solo con el .trim    
+    let emailValor = emailL     // El nombre de esta variable es igual a email
+    let passwordValor = password
 
+    if (emailValor.trim()===""||emailValor===null|| passwordValor.trim()===""||passwordValor===null) {
+        alert('llenar espacios vacios') // cambiar alerta
+        console.log("if datos");
+    }else{
+        traerDatos()
+        console.log('traerDatos()');
+    }  
+}
+
+// evento click al btnInicio
+btnInicio.addEventListener("click", validarVacios)
 
 
 
--- src/Registrarse.js
+++ src/Registrarse.js
@@ -1,30 +1,42 @@
 let btnRegistrarse = document.getElementById("btnRegistrarse")
-let nombre = document.getElementById("nombre").value
-let email = document.getElementById("email").value
-let password = document.getElementById("password").value
+let nombre = document.getElementById("nombre")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let infUsuarios= JSON.parse(localStorage.getItem("user")) || []
 
-function validarVacios() {         // Estoy validando espacios vacios
-    if (nombre.trim()===""|| email.trim()==="" || password.trim()==="") {
-        alert('llenar espacios vacios') // cambiar alerta
-    }else{
-        guardarDatos()
-    }
-}
-function guardarDatos() {   
+function guardarDatos() {
+    console.log("entra");  
     let nombre = document.getElementById("nombre").value // con el .value les estoy dando un valor
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
-     // Aca estoy guardando los datos en el localStorage
-    localStorage.setItem("nombre",nombre),
-    localStorage.setItem("email", email),
-    localStorage.setItem("password",password)   
-    // para que los datos no se elimine del localStorage lo tengo que hacer con un arreglo
+    
+    let usuario = {
+        nombre:nombre,
+        email:email,
+        password:password
+    } 
+    infUsuarios.push(usuario)
+    // Aca estoy guardando los datos en el localStorage
+    localStorage.setItem("user",JSON.stringify(infUsuarios))
 }
-  // Aca estoy dandole un evento al boton
-btnRegistrarse.addEventListener("click",
-    validarVacios
-  
-)
+
+function validarVacios() {
+      // Estoy validando espacios vacios
+    
+      let nombreValor = nombre.value     // les cree estas variables porque no me estaba funcionando con el .trim 
+    let passwordValor = password.value
+    let emailValor = email.value
+
+    if (nombreValor.trim()===""||nombreValor===null||passwordValor.trim()===""||passwordValor===null||emailValor.trim()===""||emailValor===null) {
+        console.log('pasa por aca');
+        alert('llenar espacios vacios') // cambiar alerta
+    }else{
+        guardarDatos()
+        console.log("esta llegando");
+    }
+}
+// Aca estoy dandole un evento al boton
+btnRegistrarse.addEventListener("click", ()=>{validarVacios()})
 
 
 
